549dc307111a5fbc264dc8a16e0d0cafd0025168
Johan Koen <johan.koen@live.co.za>
2025-07-20 22:35:24 +0000

    lots of changes

--- restart.sh
+++ restart.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/bash
-#
-cd /root/ourlife-app/
+cd /var/www/ourlife-app/
 git pull
-pm2 delete ourlife-app-api-server ourlife-app-web
-for i in `ps aux | grep node | grep -v grep | awk '{print $2}' ; ` ; do kill -9 $i ; done
-systemctl restart nginx 
+npm install
+pm2 stop ourlife-app-api-server
+systemctl restart nginx
 sleep 1
-#
-pm2 start $(which http-server) --name ourlife-app-web -- -p 8080 -d /root/ourlife-app --watch --watch-delay 1000
-pm2 start --name ourlife-app-api-server /root/ourlife-app/server.js --watch --watch-delay 1000
+export NODE_NO_WARNINGS=1
+pm2 start --name ourlife-app-api-server /var/www/ourlife-app/server.js --watch --watch-delay 1000 -- --port 3000
+pm2 save
