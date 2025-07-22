951b6027030aa4907ccb1aa7382d15b5e5b70ca4
Johan Koen <johan.koen@live.co.za>
2025-07-22 18:25:04 +0200

    Moved to prod - this one added correct script dir - WORKING CODE

--- restart.sh
+++ restart.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/bash
 #
-cd /root/ourlife-app/
 git pull
 pm2 delete ourlife-app-api-server ourlife-app-web
 for i in `ps aux | grep node | grep -v grep | awk '{print $2}' ; ` ; do kill -9 $i ; done
@@ -9,5 +8,5 @@ sleep 1
 sleep 1
 npm cache clean --force
 #
-pm2 start $(which http-server) --name ourlife-app-web -- -p 8080 -d /root/ourlife-app
-pm2 start --name ourlife-app-api-server /root/ourlife-app/server.js 
+pm2 start $(which http-server) --name ourlife-app-web -- -p 8080 -d . 
+pm2 start --name ourlife-app-api-server server.js 
