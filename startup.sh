658a1246d36e5715133fe78a2d8c2971955d49bf
Johan Koen <johan.koen@live.co.za>
2025-07-22 19:00:31 +0200

    deleted stuff

--- startup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-#
-pm2 start $(which http-server) --name ourlife-app-web -- -p 8080 -d /root/ourlife-app --watch --watch-delay 1000
-pm2 start --name ourlife-app-api-server /root/ourlife-app/server.js --watch --watch-delay 1000
